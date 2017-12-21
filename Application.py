33d2fa7a271deac2480c3a1c1dabcd655b9f413e
Felipe <marcelus20felipe@gmail.com>
2017-12-21 20:36:29 +0000

    THE METHOD GET VALUE GETS THE CURRENT BITCOIN VALUE

--- Application.py
+++ Application.py
@@ -1,20 +1,19 @@
-import tkinter as tk
-from matplotlib import pyplot as plt
-from tkinter import ttk
+import requests
+
+class BC_data_ret:
+    def __init__(self):
+        pass
+
+    def get_value(self, currency, amount):
+
+        link = "https://blockchain.info/tobtc?currency={}&value={}".format(currency, amount)
+
+        content = requests.get(link).content
+        content = str(content, "utf-8")
+
+        btc_price = float(amount)/float(content)
+        print(content, btc_price)
 
 
-class BTCApp(tk.Tk):
-
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-
-        container = tk.Frame(side="top", sticky="nswe");
-
-class 
-
-
-
-#app = BTCApp()
-#app.mainloop()
-
-
+bc = BC_data_ret()
+bc.get_value("EUR", "1")
