ba1291e5f7109c4d3e4be50aec6e1986dcdb2b12
Felipe <marcelus20felipe@gmail.com>
2017-12-21 20:17:46 +0000

    project initialized

---
+++ Application.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from matplotlib import pyplot as plt
+from tkinter import ttk
+
+
+class BTCApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        container = tk.Frame(side="top", sticky="nswe");
+
+class 
+
+
+
+#app = BTCApp()
+#app.mainloop()
+
+
